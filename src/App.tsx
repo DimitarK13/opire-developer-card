d4162c2e631d2fe7ccf57559d5c3c5ea68ce58d0
DimitarK13 <dimikalapocev@gmail.com>
2024-12-06 19:09:43 +0100

    feat(app): render cards with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
+import Card from './components/Card';
+import { developers } from './developers.ts';
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [developerIndex, setDeveloperIndex] = useState(0);
+
+  const handleChange = (id: number) => {
+    setDeveloperIndex(id);
+  };
+
+  return (
+    <main>
+      <Card developer={developers[developerIndex]} />
+
+      <div className="developers">
+        {developers.map((dev, index) => (
+          <button key={index} onClick={() => handleChange(index)}>
+            {dev.name}
+          </button>
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default App;

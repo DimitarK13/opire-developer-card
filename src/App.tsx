260b6e401ebebe93d5e8563f8cf389246acc17c3
DimitarK13 <dimikalapocev@gmail.com>
2024-12-07 18:26:53 +0100

    feat(global): light/dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,25 @@ import { developers } from './developers.ts';
 
 function App() {
   const [developerIndex, setDeveloperIndex] = useState(0);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   const handleChange = (id: number) => {
     setDeveloperIndex(id);
   };
 
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevValue) => !prevValue);
+  };
+
   return (
-    <main>
+    <main className={`theme--${isDarkMode ? 'dark' : 'light'}`}>
       <Card developer={developers[developerIndex]} />
 
       <div className="developers">
+        <h5>Toggle Dark Mode</h5>
+        <button onClick={toggleDarkMode}>Toggle</button>
+
+        <h5>Change Developer</h5>
         {developers.map((dev, index) => (
           <button key={index} onClick={() => handleChange(index)}>
             {dev.name}
